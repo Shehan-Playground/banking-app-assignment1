18add3b61ee110a14d5449f9f41f58e16fa5230e
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-15 22:47:29 +0530

    Dashboard created

---
+++ BankingApp.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BankingApp {
+    private static Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
+
+        final String BLUE_COLOR = "\033[034m";
+        final String RED_COLOR = "\033[031m";
+        final String BOLD = "\033[1m";
+        final String RESET = "\033[0m";
+        final String CLEAR = "\033[H\033[2J";
+
+        final String APP_MARGIN = ".repeat((50-%s.length())/2).concat(%s)";
+        
+        final String DASHBOARD = "💰 Welcome to Smart Banking App";
+        final String OPEN_ACCOUNT = "Open New Account";
+        final String DEPOSIT_MONEY = "Deposit Money";
+        final String WITHDRAW_MONEY = "Withdraw Money";
+        final String TRANSFER_MONEY = "Transfer Money";
+        final String CHECK_BALANCE = "Check Account Balance";
+        final String DROP_ACCOUNT = "Drop Existing Account";
+
+        String[] accountNum = {"SDB-00001","SDB-00002","SDB-00002"};
+        String[] accountName = {"Suresh Mahanama","Sanath Jayasuriya","Avishka Gunawardene"};
+        String[] accountBalance = {"50000","75000","95000"};
+
+        String screen = DASHBOARD;
+    
+        // do {
+            final String APPTITLE = screen;
+            System.out.println(CLEAR);
+            System.out.println(BLUE_COLOR);
+            System.out.println(BOLD);
+            System.out.printf(" ".repeat((50-APPTITLE.length())/2).concat(APPTITLE));
+            System.out.println(RESET);
+
+            switch (screen){
+                case DASHBOARD:
+                    // System.out.println(COLOR);
+                    System.out.printf("\n\t[1]. %s\n\t[2]. %s\n\t[3]. %s\n\t[4]. %s\n\t[5]. %s\n\t[6]. %s\n\t[7]. Exit\n", OPEN_ACCOUNT,DEPOSIT_MONEY,WITHDRAW_MONEY,TRANSFER_MONEY,CHECK_BALANCE,DROP_ACCOUNT);
+                    System.out.print("\n\n\tEnter an option: ");
+                    int option = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println(RESET);
+                    System.out.println();
+                    System.out.println();
+
+                    switch (option) {
+                        case 1: screen = OPEN_ACCOUNT; break;
+                        case 2: screen = DEPOSIT_MONEY; break;
+                        case 3: screen = WITHDRAW_MONEY; break;
+                        case 4: screen = TRANSFER_MONEY; break;
+                        case 5: screen = CHECK_BALANCE; break;
+                        case 6: screen = DROP_ACCOUNT; break;
+                        case 7: System.exit(0); break;
+                        default: ;
+                    }
+                    break;
+                
+
+
+    }
+}
+
+}
